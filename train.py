1acfa541fa4b5b2b18ce8ea32ace8e7ed108e537
Kyle Rode <52089154+mytech-today-now@users.noreply.github.com>
2026-05-30 12:25:03 -0500

    halve TOTAL_BATCH_SIZE to 2^16 (more steps)

--- train.py
+++ train.py
@@ -802,7 +802,7 @@ HEAD_DIM = 64            # target head dimension for attention
 WINDOW_PATTERN = "SSSL"   # sliding window pattern: L=full, S=half context
 
 # Optimization
-TOTAL_BATCH_SIZE = 2 ** 17
+TOTAL_BATCH_SIZE = 2 ** 16
 EMBEDDING_LR = 0.6
 UNEMBEDDING_LR = 0.004
 MATRIX_LR = 0.04
